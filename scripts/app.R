8b9bcb42aee298d54a1c1b4fe7f358b17cc54471
Sondre Ulvund Solstad <sondresolstad@economist.com>
2023-02-04 18:25:07 +0000

    now has functioning app and no reintroduction of banned words

---
+++ scripts/app.R
@@ -0,0 +1,42 @@
+library(shiny)
+
+# Only run examples in interactive R sessions
+if (interactive()) {
+
+  
+  sensitive_words <- c('frog', 'bread')
+
+  ui <- fluidPage(
+    textAreaInput("text", "", "My name is frog and I am the best bread"),
+    textInput("sensitive_words", "Censored words", paste(sensitive_words, collapse = ', ')),
+    textInput("dictionary", "Dictionary", "Dictionary"),
+    verbatimTextOutput("value")
+  )
+  server <- function(input, output) {
+    
+#    dat <- reactiveValues(trythis = NA)
+    
+    # Generate dummy dictionary
+#    input$dictionary <- data.frame(pinyin = c("A", "A", "A", "B", "B"),
+#                             word = c("frog", "submarine", 
+#                                      "bird", "cake", "bread"))
+#    input$sensitive_words <- c('frog', 'bread')
+#    dat$trythis <- 'LOLOLOL'
+    
+    source('scramble.R')
+    
+    dictionary <- data.frame(sound = c("A", "A", "A", "B", "B", "A"),
+                      word = c("frog", "submarine", 
+                               "bird", "cake", "bread", "synonym"))
+    
+    from_csv_to_vector <- function(x){gsub(' ', '', unlist(strsplit(x, ',')))}
+
+    output$value <- renderText({scramble(input$text,
+                                         dictionary,
+                                         sensitive = from_csv_to_vector(input$sensitive_words))})
+#    output$value <- renderText({ scramble(text = input$input, 
+#                                          dic = dictionary,
+#                                          })
+  }
+  shinyApp(ui, server)
+}
--- scripts/scramble.R
+++ scripts/scramble.R
@@ -1,29 +1,23 @@
-# This program replaces characters with other characters, based on a common sound (pinyin).
-
-# Set locale to Chinese
-Sys.setlocale(category = "LC_ALL", locale = "chs") 
-
-# Import dictionary (source: https://www.mdbg.net/chinese/dictionary?page=cedict)
-dic <- read.csv('source-data/cedict_ts.u8')
-
-
-# Generate dummy dictionary
-dic <- data.frame(pinyin = c("A", "A", "A", "B", "B"),
-                  word = c("frog", "submarine", 
-                           "bird", "cake", "bread"))
-
-# Generate dummy text
-text <- "I am a frog, and I like blogs. Frog is not the answer. However, I wish it could be, at some point, fRogs, that is. Or birds for that matter. BTW, what about bread"
+# This program replaces words with other words, based on a common sound.
 
 scramble <- function(text, dic, 
                      sensitive = c('frog', 'bread')){
   
   # Build possible replacements (can potentially build in probability vector here if needed)
   replacements <- list()
-    for(i in 1:length(sensitive)){
-    replacements[[i]] <- setdiff(dic$word[dic$pinyin == dic$pinyin[dic$word == sensitive[i]]], sensitive[i])
+  
+  if(sum(!sensitive %in% dic$word) != 0){
+    stop('Stopping: ', sensitive[!sensitive %in% dic$word], ' not in dictionary.')
+  }
+  
+  # Ensure banned words cannot be used as replacements
+  dic$can_replace <- ifelse(!dic$word %in% sensitive, T, F)
+  
+  # Generate replacement words
+  for(i in 1:length(sensitive)){
+    replacements[[i]] <- setdiff(dic$word[dic$sound == dic$sound[dic$word == sensitive[i]] & dic$can_replace], sensitive[i])
     if(length(replacements[[i]]) == 0){
-      stop(paste0('Stopping: ', sensitive[i], ' has not eligible homonyms.'))
+      stop(paste0('Stopping: ', sensitive[i], ' has no eligible homonyms.'))
     }
   }
   
@@ -41,5 +35,25 @@ scramble <- function(text, dic,
   return(text)
 }
 
+if(F){
+  
+  # Set locale to Chinese
+  Sys.setlocale(category = "LC_ALL", locale = "chs") 
+  
+  # Import dictionary (source: https://www.mdbg.net/chinese/dictionary?page=cedict)
+  dic <- read.csv('source-data/cedict_ts.u8')
+  
+  
+  
+  # Generate dummy dictionary
+  dic <- data.frame(sound = c("A", "A", "A", "B", "B"),
+                    word = c("frog", "submarine", 
+                             "bird", "cake", "bread"))
+  sensitive_words <- c('frog', 'bread')
+  
+  # Generate dummy text
+  text <- "I am a frog, and I like blogs. Frog is not the answer. However, I wish it could be, at some point, fRogs, that is. Or birds for that matter. BTW, what about bread"
+  
+  
 scramble(text, dic)
-
+}
