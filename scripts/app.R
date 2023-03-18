cbfaeb22255fb4a8ac49538f897875942d4c371c
Sondre Ulvund Solstad <sondresolstad@economist.com>
2023-03-18 17:45:37 +0000

    dark mode added

--- scripts/app.R
+++ scripts/app.R
@@ -1,17 +1,95 @@
 library(shiny)
+library(shinythemes)
 
 # Only run examples in interactive R sessions
 if (interactive()) {
 
   Sys.setlocale(category = "LC_ALL", locale = "chs") 
   
-  sensitive_words <- c('frog', 'bread')
+  # sensitive_words <- c('习', '近', '平', '乔', '拜', '登')
 
   ui <- fluidPage(
+    theme = shinytheme("darkly"),
+    tags$head(
+      tags$style(
+        "#themeToggle, 
+            .visually-hidden {
+                position: absolute;
+                width: 1px;
+                height: 1px;
+                clip: rect(0 0 0 0);
+                clip: rect(0, 0, 0, 0);
+                overflow: hidden;
+            }",
+        "#themeToggle + span .fa-sun {
+                font-size: 16pt;
+            }"
+      )
+    ),
+    sidebarPanel("Censored terms",
+    textAreaInput("sensitive_words", "", "习, 近" #paste(sensitive_words, collapse = ', ')
+              )),
+    sidebarPanel(
+    "Input",
     textAreaInput("text", "", "My name is frog and I am the best bread"),
-    textInput("sensitive_words", "Censored words", paste(sensitive_words, collapse = ', ')),
-    textInput("dictionary", "Dictionary", "Dictionary"),
-    verbatimTextOutput("value")
+    "Output",
+    "",
+    # verbatimTextOutput("value"),
+    textOutput("value")),
+    checkboxInput(
+      inputId = "themeToggle",
+      label = icon("sun")
+    ),tags$script(
+      "
+        // define css theme filepaths
+        const themes = {
+            dark: 'shinythemes/css/darkly.min.css',
+            light: 'shinythemes/css/flatly.min.css'
+        }
+
+        // function that creates a new link element
+        function newLink(theme) {
+            let el = document.createElement('link');
+            el.setAttribute('rel', 'stylesheet');
+            el.setAttribute('text', 'text/css');
+            el.setAttribute('href', theme);
+            return el;
+        }
+
+        // function that remove <link> of current theme by href
+        function removeLink(theme) {
+            let el = document.querySelector(`link[href='${theme}']`)
+            return el.parentNode.removeChild(el);
+        }
+
+        // define vars
+        const darkTheme = newLink(themes.dark);
+        const lightTheme = newLink(themes.light);
+        const head = document.getElementsByTagName('head')[0];
+        const toggle = document.getElementById('themeToggle');
+
+        // define extra css and add as default
+        const extraDarkThemeCSS = '.dataTables_length label, .dataTables_filter label, .dataTables_info {       color: white!important;} .paginate_button { background: white!important;} thead { color: white;}'
+        const extraDarkThemeElement = document.createElement('style');
+        extraDarkThemeElement.appendChild(document.createTextNode(extraDarkThemeCSS));
+        head.appendChild(extraDarkThemeElement);
+
+
+        // define event - checked === 'light'
+        toggle.addEventListener('input', function(event) {
+            // if checked, switch to light theme
+            if (toggle.checked) {
+                removeLink(themes.dark);
+                head.removeChild(extraDarkThemeElement);
+                head.appendChild(lightTheme);
+            }  else {
+                // else add darktheme
+                removeLink(themes.light);
+                head.appendChild(extraDarkThemeElement)
+                head.appendChild(darkTheme);
+            }
+        })
+        ")
   )
   
   server <- function(input, output) {
